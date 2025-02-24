1a2f5659c37575625b4035617196309f71b8edef
dao huy tuan <tuandh95@gmail.com>
2025-02-24 14:07:47 +0700

    Initial Commit

---
+++ LangSwitcher/LangSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangSwitcherApp.swift
+//  LangSwitcher
+//
+//  Created by zyye on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LangSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
